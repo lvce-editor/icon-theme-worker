dab456af67e77d95913a5cd7f0047ba5ca498291
Le Vivilet <levivilet@pm.me>
2025-09-10 22:16:08 +0200

    feature: export more icon theme commands

--- packages/icon-theme-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/icon-theme-worker/src/parts/CommandMap/CommandMap.ts
@@ -1,7 +1,9 @@
-import { getIcons } from '../GetIcon/GetIcon.ts'
+import { getFileIcon, getFolderIcon, getIcons } from '../GetIcon/GetIcon.ts'
 import { getIconThemeJson } from '../GetIconThemeJson/GetIconThemeJson.ts'
 
 export const commandMap = {
   'IconTheme.getIcons': getIcons,
+  'IconTheme.getFileIcon': getFileIcon,
+  'IconTheme.getFolderIcon': getFolderIcon,
   'IconTheme.getIconThemeJson': getIconThemeJson,
 }
