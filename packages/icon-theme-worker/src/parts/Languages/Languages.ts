f435b7e2fd6f44e79a64942760aa1ba98e3daf3d
Le Vivilet <levivilet@pm.me>
2025-09-10 23:30:55 +0200

    feature: add languages module (#25)

---
+++ packages/icon-theme-worker/src/parts/Languages/Languages.ts
@@ -0,0 +1,30 @@
+import * as GetFileExtension from '../GetFileExtension/GetFileExtension.ts'
+
+const state = {
+  languages: Object.create(null),
+}
+
+export const addLanguages = (languages: readonly any[]): void => {
+  for (const language of languages) {
+    const { id, extensions } = language
+    for (const extension of extensions) {
+      state.languages[extension] = id
+    }
+  }
+}
+
+export const getLanguageId = (fileName: string): string => {
+  const { languages } = state
+  const extensionIndex = GetFileExtension.getFileExtensionIndex(fileName)
+  const extension = fileName.slice(extensionIndex)
+  const extensionLower = extension.toLowerCase()
+  if (extensionLower in languages) {
+    return languages[extensionLower]
+  }
+  const secondExtensionIndex = GetFileExtension.getNthFileExtension(fileName, extensionIndex - 1)
+  const secondExtension = fileName.slice(secondExtensionIndex)
+  if (secondExtensionIndex !== -1 && secondExtension in languages) {
+    return languages[secondExtension]
+  }
+  return ''
+}
