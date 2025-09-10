d5eeb937f86540e802694eb849cb4d5f66c5b553
Le Vivilet <levivilet@pm.me>
2025-09-10 21:58:00 +0200

    feature: register icon theme command (#11)

--- packages/icon-theme-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/icon-theme-worker/src/parts/CommandMap/CommandMap.ts
@@ -1,5 +1,7 @@
 import { getIcons } from '../GetIcon/GetIcon.ts'
+import { getIconThemeJson } from '../GetIconThemeJson/GetIconThemeJson.ts'
 
 export const commandMap = {
   'IconTheme.getIcons': getIcons,
+  'IconTheme.getIconThemeJson': getIconThemeJson,
 }
