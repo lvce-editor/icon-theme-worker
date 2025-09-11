82b59fdf708755284e538c1c90be72eef5f7b8db
Le Vivilet <levivilet@pm.me>
2025-09-11 15:14:01 +0200

    feature: simplify absolute icon path (#52)

--- packages/icon-theme-worker/src/parts/GetAbsoluteIconPath/GetAbsoluteIconPath.ts
+++ packages/icon-theme-worker/src/parts/GetAbsoluteIconPath/GetAbsoluteIconPath.ts
@@ -1,6 +1,4 @@
-import * as GetRemoteSrc from '../GetRemoteSrc/GetRemoteSrc.ts'
 import * as IconThemeState from '../IconThemeState/IconThemeState.ts'
-import * as PathSeparator from '../PathSeparator/PathSeparator.ts'
 
 export const getAbsoluteIconPath = (iconTheme: any, icon: string): string => {
   if (!iconTheme) {
@@ -8,15 +6,8 @@ export const getAbsoluteIconPath = (iconTheme: any, icon: string): string => {
   }
   const result = iconTheme.iconDefinitions[icon]
   const baseUrl = IconThemeState.getExtensionBaseUrl()
-  if (result) {
-    if (baseUrl.startsWith('http://') || baseUrl.startsWith('https://')) {
-      return `${baseUrl}${result}`
-    }
-    if (baseUrl.includes(PathSeparator.BackSlash)) {
-      const extensionUri = baseUrl.replaceAll(PathSeparator.BackSlash, PathSeparator.Slash)
-      return `/remote/${extensionUri}${result}`
-    }
-    return GetRemoteSrc.getRemoteSrc(`${baseUrl}${result}`)
+  if (result && baseUrl) {
+    return `${baseUrl}${result}`
   }
   return ''
 }
