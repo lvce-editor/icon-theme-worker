afc35de5ae612b6252a8bf78e74f737d093809be
Le Vivilet <levivilet@pm.me>
2025-09-11 08:31:18 +0200

    feature: sort commands

--- packages/icon-theme-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/icon-theme-worker/src/parts/CommandMap/CommandMap.ts
@@ -3,9 +3,9 @@ import { loadIconThemeJson } from '../GetIconThemeJson/GetIconThemeJson.ts'
 import { addLanguages } from '../Languages/Languages.ts'
 
 export const commandMap = {
+  'IconTheme.addLanguages': addLanguages,
   'IconTheme.getFileIcon': getFileIcon,
   'IconTheme.getFolderIcon': getFolderIcon,
   'IconTheme.getIcons': getIcons,
   'IconTheme.getIconThemeJson': loadIconThemeJson,
-  'IconTheme.addLanguages': addLanguages,
 }
