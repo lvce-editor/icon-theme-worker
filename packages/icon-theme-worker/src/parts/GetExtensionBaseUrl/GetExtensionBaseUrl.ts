e225e1cb2c2a6a728a084142d84aa3b192806234
Le Vivilet <levivilet@pm.me>
2025-09-11 22:18:29 +0200

    feature: support icon themes in electron with custom protocol (#53)

--- packages/icon-theme-worker/src/parts/GetExtensionBaseUrl/GetExtensionBaseUrl.ts
+++ packages/icon-theme-worker/src/parts/GetExtensionBaseUrl/GetExtensionBaseUrl.ts
@@ -1,8 +1,12 @@
 import * as PlatformType from '../PlatformType/PlatformType.ts'
 
 export const getExtensionBaseUrl = (webExtension: any, platform: number): string => {
-  // TODO support file scheme and application scheme
-  if (webExtension.uri && webExtension.uri.startsWith('file://') && platform === PlatformType.Web) {
+  if (webExtension.uri && webExtension.uri.startsWith('file://') && platform === PlatformType.Remote) {
+    const relative = webExtension.uri.slice('file://'.length)
+    return `/remote${relative}`
+  }
+  if (webExtension.uri && webExtension.uri.startsWith('file://') && platform === PlatformType.Electron) {
+    // TODO for electron use `/file` urls
     const relative = webExtension.uri.slice('file://'.length)
     return `/remote${relative}`
   }
