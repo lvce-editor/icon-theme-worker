c339f55eca1e34c6ba3a6547c0757984fbb74d53
Le Vivilet <levivilet@pm.me>
2025-09-15 16:11:44 +0200

    feature: add icon theme e2e test

---
+++ packages/e2e/src/icon-theme.change.ts
@@ -0,0 +1,20 @@
+export const name = 'icon-theme.change'
+
+export const skip = true
+
+export const test = async ({ FileSystem, Workspace, Extension, IconTheme, Locator, expect, BaseUrl }) => {
+  // arrange
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(`${tmpDir}/test.xyz`, 'test')
+  await FileSystem.mkdir(`${tmpDir}/test-folder`)
+  await Workspace.setPath(tmpDir)
+  await Extension.addWebExtension(new URL('../fixtures/sample.icon-theme', import.meta.url).toString())
+
+  // act
+  await IconTheme.setIconTheme('test-icon-theme')
+
+  // assert
+  const iconFile = Locator('.TreeItem[aria-label="test.xyz"] .FileIcon')
+  const baseUrl = BaseUrl.getBaseUrl()
+  await expect(iconFile).toHaveAttribute('src', `${baseUrl}packages/extension-host-worker-tests/fixtures/sample.icon-theme/icons/default_file.svg`)
+}
