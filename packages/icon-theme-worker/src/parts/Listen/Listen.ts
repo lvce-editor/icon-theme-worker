346c708a8b88860ef6b28d91a9ccfaad052bdda7
Le Vivilet <levivilet@pm.me>
2025-09-27 12:47:51 +0200

    feature: add commandMapRef

--- packages/icon-theme-worker/src/parts/Listen/Listen.ts
+++ packages/icon-theme-worker/src/parts/Listen/Listen.ts
@@ -1,7 +1,9 @@
 import { WebWorkerRpcClient } from '@lvce-editor/rpc'
 import * as CommandMap from '../CommandMap/CommandMap.ts'
+import { commandMapRef } from '../CommandMapRef/CommandMapRef.ts'
 
 export const listen = async (): Promise<void> => {
+  Object.assign(commandMapRef, CommandMap.commandMap)
   await WebWorkerRpcClient.create({
     commandMap: CommandMap.commandMap,
   })
