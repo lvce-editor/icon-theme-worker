e7036a975f49862587a83a87a65acbde7e35771e
Le Vivilet <levivilet@pm.me>
2025-10-23 17:05:24 +0200

    feature: reuse string (#81)

--- packages/icon-theme-worker/src/parts/GetExtensionBaseUrl/GetExtensionBaseUrl.ts
+++ packages/icon-theme-worker/src/parts/GetExtensionBaseUrl/GetExtensionBaseUrl.ts
@@ -1,13 +1,15 @@
 import { PlatformType } from '@lvce-editor/constants'
 
+const fileProtocol = 'file://'
+
 export const getExtensionBaseUrl = (webExtension: any, platform: number): string => {
-  if (webExtension.uri && webExtension.uri.startsWith('file://') && platform === PlatformType.Remote) {
-    const relative = webExtension.uri.slice('file://'.length)
+  if (webExtension.uri && webExtension.uri.startsWith(fileProtocol) && platform === PlatformType.Remote) {
+    const relative = webExtension.uri.slice(fileProtocol.length)
     return `/remote${relative}`
   }
-  if (webExtension.uri && webExtension.uri.startsWith('file://') && platform === PlatformType.Electron) {
+  if (webExtension.uri && webExtension.uri.startsWith(fileProtocol) && platform === PlatformType.Electron) {
     // TODO for electron use `/file` urls
-    const relative = webExtension.uri.slice('file://'.length)
+    const relative = webExtension.uri.slice(fileProtocol.length)
     return `/remote${relative}`
   }
   return webExtension.path
