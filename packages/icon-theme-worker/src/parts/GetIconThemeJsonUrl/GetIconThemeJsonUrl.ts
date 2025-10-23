5df159f7c3c2ff24eae9497dc5e77e7e3492f181
Le Vivilet <levivilet@pm.me>
2025-10-23 17:24:09 +0200

    feature: simplify url (#84)

--- packages/icon-theme-worker/src/parts/GetIconThemeJsonUrl/GetIconThemeJsonUrl.ts
+++ packages/icon-theme-worker/src/parts/GetIconThemeJsonUrl/GetIconThemeJsonUrl.ts
@@ -1,14 +1,5 @@
 import { joinPath } from '../JoinPath/JoinPath.ts'
 
-export const getIconThemeJsonUrl = (iconThemeExtension: any, locationProtocol: string, locationHost: string): string => {
-  const relativePath = iconThemeExtension.path
-  if (iconThemeExtension && iconThemeExtension.extensionUri && iconThemeExtension.extensionUri.startsWith('file://')) {
-    const rest = `${iconThemeExtension.extensionUri}`.slice('file://'.length)
-    const protocol = locationProtocol
-    const path = joinPath('/remote', rest, relativePath)
-    return `${protocol}://${locationHost}${path}`
-  }
-  const rest = iconThemeExtension.extensionPath
-  const path = joinPath(rest, relativePath)
-  return `${iconThemeExtension.extensionPath}/${path}`
+export const getIconThemeJsonUrl = (iconThemeExtension: any): string => {
+  return joinPath(iconThemeExtension.extensionRemoteUri, iconThemeExtension.path)
 }
