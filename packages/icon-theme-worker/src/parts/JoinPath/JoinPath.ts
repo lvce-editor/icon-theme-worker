3a8e81a0c31a6643a9d3520fb0ea47d996c8686b
Le Vivilet <levivilet@pm.me>
2025-10-23 17:23:44 +0200

    feature: improve path join (#83)

--- packages/icon-theme-worker/src/parts/JoinPath/JoinPath.ts
+++ packages/icon-theme-worker/src/parts/JoinPath/JoinPath.ts
@@ -5,6 +5,8 @@ export const joinPath = (...parts: readonly string[]): string => {
   for (const part of parts) {
     if (merged.endsWith(Character.Slash) && part.startsWith(Character.Slash)) {
       merged += part.slice(1)
+    } else if (!merged.endsWith(Character.Slash) && !part.startsWith(Character.Slash)) {
+      merged += Character.Slash + part
     } else {
       merged += part
     }
