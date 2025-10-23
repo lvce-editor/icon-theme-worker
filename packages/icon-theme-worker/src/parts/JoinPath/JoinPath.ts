9fd71ef8fa5da8a6872592d4d1e41ea8d10d8ae5
Le Vivilet <levivilet@pm.me>
2025-10-23 17:16:31 +0200

    feature: add getJsonUrl function (#82)

---
+++ packages/icon-theme-worker/src/parts/JoinPath/JoinPath.ts
@@ -0,0 +1,13 @@
+import * as Character from '../Character/Character.ts'
+
+export const joinPath = (...parts: readonly string[]): string => {
+  let merged = ''
+  for (const part of parts) {
+    if (merged.endsWith(Character.Slash) && part.startsWith(Character.Slash)) {
+      merged += part.slice(1)
+    } else {
+      merged += part
+    }
+  }
+  return merged
+}
