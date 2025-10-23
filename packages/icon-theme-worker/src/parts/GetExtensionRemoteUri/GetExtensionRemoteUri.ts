b0c839aed4763570c7a2adb19ca95fef1865d5f7
Le Vivilet <levivilet@pm.me>
2025-10-23 17:45:30 +0200

    feature: add todo

--- packages/icon-theme-worker/src/parts/GetExtensionRemoteUri/GetExtensionRemoteUri.ts
+++ packages/icon-theme-worker/src/parts/GetExtensionRemoteUri/GetExtensionRemoteUri.ts
@@ -1,5 +1,7 @@
 import { PlatformType } from '@lvce-editor/constants'
 
+// TODO for electron use `/file` urls
+
 export const getExtensionRemoteUri = (extensionUri: string, extensionPath: string, platform: number): string => {
   if (platform === PlatformType.Remote || (platform === PlatformType.Electron && extensionUri && extensionUri.startsWith('file://'))) {
     const rest = extensionUri.slice('file://'.length)
