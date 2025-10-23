efefa81b6b14b2b0ce35453272e7c1d7c7693fc0
Le Vivilet <levivilet@pm.me>
2025-10-23 17:33:48 +0200

    feature: add interface

--- packages/icon-theme-worker/src/parts/DoGetIconThemeJson/DoGetIconThemeJson.ts
+++ packages/icon-theme-worker/src/parts/DoGetIconThemeJson/DoGetIconThemeJson.ts
@@ -1,18 +1,25 @@
 import { PlatformType } from '@lvce-editor/constants'
+import type { LoadedIconTheme } from '../LoadedIconTheme/LoadedIconTheme.ts'
 import * as FindMatchingIconThemeExtension from '../FindMatchingIconThemeExtension/FindMatchingIconThemeExtension.ts'
 import { getIconThemeJsonUrl } from '../GetIconThemeJsonUrl/GetIconThemeJsonUrl.ts'
 import * as GetIconThemeUrl from '../GetIconThemeUrl/GetIconThemeUrl.ts'
 import * as GetJson from '../GetJson/GetJson.ts'
 
-export const doGetIconThemeJson = async (extensions: readonly any[], iconThemeId: string, assetDir: string, platform: number): Promise<any> => {
+export const doGetIconThemeJson = async (
+  extensions: readonly any[],
+  iconThemeId: string,
+  assetDir: string,
+  platform: number,
+): Promise<LoadedIconTheme | undefined> => {
   if (platform === PlatformType.Web) {
     const url = GetIconThemeUrl.getIconThemeUrl(assetDir, iconThemeId)
     const json = await GetJson.getJson(url)
     return {
-      json,
-      extensionPath: `${assetDir}/extensions/builtin.${iconThemeId}`,
       extensionBaseUrl: `${assetDir}/extensions/builtin.${iconThemeId}`,
       extensionRemoteUri: `${assetDir}/extensions/builtin.${iconThemeId}`,
+      extensionPath: `${assetDir}/extensions/builtin.${iconThemeId}`,
+      extensionUri: `${assetDir}/extensions/builtin.${iconThemeId}`,
+      json,
     }
   }
   const iconTheme = FindMatchingIconThemeExtension.findMatchingIconThemeExtension(extensions, iconThemeId, platform)
@@ -22,9 +29,10 @@ export const doGetIconThemeJson = async (extensions: readonly any[], iconThemeId
   const iconThemeUrl = getIconThemeJsonUrl(iconTheme)
   const iconThemeJson = await GetJson.getJson(iconThemeUrl)
   return {
-    extensionUri: iconTheme.extensionUri || '',
-    extensionRemoteUri: iconTheme.extensionRemoteUri || '',
     extensionPath: iconTheme.extensionPath,
+    extensionRemoteUri: iconTheme.extensionRemoteUri || '',
+    extensionUri: iconTheme.extensionUri || '',
+    extensionBaseUrl: iconTheme.extensionRemoteUri || '',
     json: iconThemeJson,
   }
 }
