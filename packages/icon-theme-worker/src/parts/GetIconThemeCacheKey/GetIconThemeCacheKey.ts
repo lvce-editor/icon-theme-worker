2c4d5aaa58159c785ec27e4d8c08326e33f45045
Le Vivilet <levivilet@pm.me>
2025-12-03 14:25:07 +0100

    feature: update icon theme etag

--- packages/icon-theme-worker/src/parts/GetIconThemeCacheKey/GetIconThemeCacheKey.ts
+++ packages/icon-theme-worker/src/parts/GetIconThemeCacheKey/GetIconThemeCacheKey.ts
@@ -1,14 +1,23 @@
-import { hash } from '../Hash/Hash.ts'
 import { supportsNormalCacheKey } from '../SupportsNormalCacheKey/SupportsNormalCacheKey.ts'
 
-const getMarkdownCacheHash = async (etag: string): Promise<string> => {
-  const contents = `${etag}:`
-  return hash(contents)
+const RE_WEAK = /^W\/"(.*)"$/
+const RE_STRONG = /^"(.*)"$/
+
+const getCacheHash = (etag: string): string => {
+  const weakMatch = etag.match(RE_WEAK)
+  if (weakMatch) {
+    return weakMatch[1]
+  }
+  const strongMatch = etag.match(RE_STRONG)
+  if (strongMatch) {
+    return strongMatch[1]
+  }
+  return etag
 }
 
 export const getIconThemeCacheKey = async (etag: string): Promise<string> => {
   const locationProtocol = location.protocol
-  const hash = await getMarkdownCacheHash(etag)
+  const hash = getCacheHash(etag)
   if (supportsNormalCacheKey(locationProtocol)) {
     return `/icon-themes/${hash}`
   }
