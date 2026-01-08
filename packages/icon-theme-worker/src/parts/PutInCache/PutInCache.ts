396e9108cbccac2b7d55c8af075e746f86c1dadf
Le Vivilet <levivilet@pm.me>
2026-01-08 22:20:03 +0100

    feature: when putting response in cache, construct response manually to void timestamp issue

--- packages/icon-theme-worker/src/parts/PutInCache/PutInCache.ts
+++ packages/icon-theme-worker/src/parts/PutInCache/PutInCache.ts
@@ -1,5 +1,20 @@
 import type { ICache } from '../ICache/ICache.ts'
 
+const parseContentLength = (response: Response): number => {
+  const raw = response.headers.get('Content-Length')
+  if (raw) {
+    return Number.parseInt(raw, 10)
+  }
+  return 1
+}
+
 export const putInCache = async (url: string, response: Response, cache: ICache): Promise<void> => {
-  await cache.put(url, response)
+  const length = parseContentLength(response)
+  const cachingResponse = new Response(response.body, {
+    headers: {
+      'Content-Length': `${length}`,
+      'Content-Type': 'application/json',
+    },
+  })
+  await cache.put(url, cachingResponse)
 }
