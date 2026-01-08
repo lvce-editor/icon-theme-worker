8dbfa46b693095edb45fb4ac1b107fb3acdedb78
Le Vivilet <levivilet@pm.me>
2026-01-08 22:54:16 +0100

    feature: when putting response in cache, construct response manually to void timestamp issue (#129)
    * feature: when putting response in cache, construct response manually to void timestamp issue
    
    * memory

--- packages/memory/src/config.ts
+++ packages/memory/src/config.ts
@@ -3,7 +3,7 @@ import { root } from './root.ts'
 
 export const threshold = 395_000
 
-export const instantiations = 3000
+export const instantiations = 3500
 
 export const instantiationsPath = join(root, 'packages', 'icon-theme-worker')
 
