fcc05eb1aa0f915305c92084c6d4c1f30e95e2f4
Le Vivilet <levivilet@pm.me>
2026-02-03 15:54:03 +0100

    fix: file icon issue

--- packages/icon-theme-worker/src/parts/GetIcon/GetIcon.ts
+++ packages/icon-theme-worker/src/parts/GetIcon/GetIcon.ts
@@ -9,7 +9,7 @@ import * as Languages from '../Languages/Languages.ts'
 import * as Logger from '../Logger/Logger.ts'
 
 const toLowerCaseMap = (map: Readonly<Record<string, string>>): Record<string, string> => {
-  return Object.fromEntries(Object.entries(map).map((entry: readonly [string, unknown]) => [entry[0].toLowerCase(), entry[1]] as const))
+  return Object.fromEntries(Object.entries(map).map((entry: readonly [string, unknown]) => [entry[0].toLowerCase(), entry[1]] as const)) as Record<string, string>
 }
 
 const getFileIconFromFileNames = (iconTheme: any, fileNameLower: string): string => {
