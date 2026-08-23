ea9f5af79a357e0ab8228e43c3854596fa1a8cf2
Le Vivilet <levivilet@pm.me>
2026-08-23 21:33:49 +0000

    feature: support disabling the icon theme

---
+++ packages/icon-theme-worker/test/GetIconThemeJson.test.ts
@@ -0,0 +1,28 @@
+import { expect, test } from '@jest/globals'
+import { loadIconThemeJson } from '../src/parts/GetIconThemeJson/GetIconThemeJson.ts'
+import { getFileIcon } from '../src/parts/GetIcon/GetIcon.ts'
+import * as IconThemeState from '../src/parts/IconThemeState/IconThemeState.ts'
+
+test('loadIconThemeJson clears the current icon theme when the id is null', async () => {
+  IconThemeState.setTheme({
+    extensionBaseUrl: '/remote',
+    extensionPath: '',
+    extensionRemoteUri: '',
+    extensionUri: '',
+    json: {
+      fileNames: {
+        'file.txt': 'text',
+      },
+      iconDefinitions: {
+        text: '/text.svg',
+      },
+    },
+  })
+
+  expect(getFileIcon({ name: 'file.txt' })).toBe('/remote/text.svg')
+
+  await loadIconThemeJson([], null, '', 0, false, '')
+
+  expect(getFileIcon({ name: 'file.txt' })).toBe('')
+  expect(IconThemeState.getExtensionBaseUrl()).toBe('')
+})
